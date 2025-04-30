204c72463057e3d9a1816d19f0c2ceb0d01e8a77
Baekangsun <ss38318@naver.com>
2025-04-30 15:58:59 +0900

    개발환경 재구축

--- src/components/Carousel.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-export default function Carousel() {
-  const [activeImage, setActiveImage] = useState();
-
-  return <></>;
-}
--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <p className='copyright'>Copyright 2025. Kangsun All rights reserved.</p>
-    </footer>
-  );
-}
